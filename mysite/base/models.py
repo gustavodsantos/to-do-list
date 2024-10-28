9c009626f9e86a37181eed28bc6ee23a78cd7a23
gustavodsantos <gustavojuniordos@hotmail.com>
2024-10-28 16:00:41 -0300

    criação de tabela no banco de de dados a partir de TDD.
    close #04

--- mysite/base/models.py
+++ mysite/base/models.py
@@ -1,5 +1,11 @@
+from django.db import models
 from django_min_custom_user.models import MinAbstractUser
 
 
 class User(MinAbstractUser):
     pass
+
+
+class Tarefa(models.Model):
+    nome = models.CharField(max_length=128)
+    feita = models.BooleanField(default=False)
