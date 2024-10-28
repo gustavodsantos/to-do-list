1bcfa4058e6400d87cf793ee18c2e81f0abefe2c
gustavodsantos <gustavojuniordos@hotmail.com>
2024-10-28 16:00:41 -0300

    criação de tabela no banco de de dados a partir de TDD.
    close #04

--- mysite/base/views.py
+++ mysite/base/views.py
@@ -1,5 +1,16 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from mysite.base.forms import TarefaNovaForm
 
 
 def home(request):
+    if request.method == 'POST':
+        form = TarefaNovaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('base:home'))
+        else:
+            return render(request, 'base/home.html', {'form': form}, status=400)
     return render(request, 'base/home.html')
