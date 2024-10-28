f3139a48eb8529954c296ecabe8f14229a1d4489
gustavodsantos <gustavojuniordos@hotmail.com>
2024-10-28 17:02:53 -0300

    listando informações do banco de dados
    close #06

--- mysite/base/views.py
+++ mysite/base/views.py
@@ -3,14 +3,18 @@ from django.shortcuts import render
 from django.urls import reverse
 
 from mysite.base.forms import TarefaNovaForm
+from mysite.base.models import Tarefa
 
 
 def home(request):
     if request.method == 'POST':
+
         form = TarefaNovaForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('base:home'))
         else:
-            return render(request, 'base/home.html', {'form': form}, status=400)
-    return render(request, 'base/home.html')
+            tarefa_pendentes = Tarefa.objects.filter(feita=False).all()
+            return render(request, 'base/home.html', {'form': form, 'tarefa_pendentes': tarefa_pendentes}, status=400)
+    tarefa_pendentes = Tarefa.objects.filter(feita=False).all()
+    return render(request, 'base/home.html', {'tarefa_pendentes': tarefa_pendentes})
