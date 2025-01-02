7b0fa53f39140479f38c9e7a9a99b080409c5339
gustavodsantos <gustavojuniordos@hotmail.com>
2025-01-01 21:42:35 -0300

    Excluído o template de edição de tarefa.

--- mysite/base/urls.py
+++ mysite/base/urls.py
@@ -6,8 +6,9 @@ app_name = 'base'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<int:tarefa_id>', views.detalhe, name='detalhe'),
-    path('apagar/<int:tarefa_id>', views.apagar, name='apagar'),
+    path('tarefa/<int:tarefa_id>/feita/', views.marcar_como_feita, name='marcar_como_feita'),
+    path('tarefa/<int:tarefa_id>/pendente/', views.marcar_como_pendente, name='marcar_como_pendente'),
+    path('tarefa/<int:tarefa_id>/apagar/', views.apagar_tarefa, name='apagar_tarefa'),
     path('logged_out/', views.logged_out, name='logged_out'),
     path('registrar/', views.registrar_usuario, name='registrar_usuario'),
 ]
